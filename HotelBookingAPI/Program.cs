3c5bf1ee76fe3ad6a1e373e283532a9f0a7be52d
Huu <n23dccn024@student.ptithcm.edu.vn>
2026-05-25 10:26:55 +0700

    Cap nhat cau hinh CORS cho phep ReactJS goi API

--- HotelBookingAPI/Program.cs
+++ HotelBookingAPI/Program.cs
@@ -8,6 +8,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); 
 
+// 1.5. KÍCH HOẠT CORS (Mở cửa cho ReactJS gọi vào)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()  // Cho phép mọi tên miền (localhost:5173, ...)
+              .AllowAnyMethod()  // Cho phép mọi hành động (GET, POST, PUT, DELETE)
+              .AllowAnyHeader(); // Cho phép mọi header
+    });
+});
+
 // 2. Kích hoạt kết nối SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -20,6 +31,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// 4. SỬ DỤNG CORS (Lưu ý: Phải đặt TRƯỚC UseAuthorization)
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 app.MapControllers();
